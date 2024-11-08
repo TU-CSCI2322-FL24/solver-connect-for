f6808374c02f5f0ef7e6c1ec5e60e60bc8c6d4f8
JossDaBoss <159561562+JossDaBoss@users.noreply.github.com>
2024-11-08 12:37:36 -0600

    Create Connect.hs

---
+++ Connect.hs
@@ -0,0 +1,13 @@
+data Piece = Empty | Player1 | Player2 deriving (Show, Ord, Eq)
+
+data Player = Player1 | Player2 deriving (Show, Eq, Ord)
+
+data GameStatus = Going | Draw | Winner Player deriving (Show, Eq, Ord)
+
+data Game = Board | CurrentPlayer Player | Status GameStatus deriving (Show, Eq, Ord)
+
+type Column = [Piece]
+
+type Board = [Column]
+
+type Point = (Int, Int)
