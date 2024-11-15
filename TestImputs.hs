43c05462405545c4e6afd2d4fb5ab7d7acacf7de
JossDaBoss <jakinkun@trinity.edu>
2024-11-15 13:16:04 -0600

    Ye

---
+++ TestImputs.hs
@@ -0,0 +1,39 @@
+import Connect
+
+board = [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]]
+
+
+ywin = [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Just Yellow, Just Yellow, Just Yellow, Just Yellow, Nothing, Nothing]]
+
+rwin = [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Just Red, Just Red, Just Red, Just Red, Nothing, Nothing]]
+
+
+real = [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Just Yellow, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Just Yellow, Nothing, Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Just Yellow, Nothing, Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Just Yellow, Nothing, Nothing, Nothing, Nothing, Nothing]]
+
+
+real2 = [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Nothing, Just Yellow, Nothing, Nothing], 
+        [Nothing, Nothing, Nothing, Just Yellow, Nothing, Nothing, Nothing],
+        [Nothing, Nothing, Just Yellow, Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Just Yellow, Nothing, Nothing, Nothing, Nothing, Nothing]]
